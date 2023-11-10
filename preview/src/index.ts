4c216c75c13562291a43b3471bcd9f64d5264981
Mauritz Dressler <mauritz@prodport.com>
2023-11-10 09:06:40 +0100

    Fix: initial errors with preview

--- preview/src/index.ts
+++ preview/src/index.ts
@@ -3,9 +3,12 @@ import http, { IncomingMessage, ServerResponse } from "http";
 import fs, { ReadStream } from "fs";
 
 /** //TODO */
-const root = "";
-const errorDocument = "";
-const previewJsPath = "";
+const root = "./src/";
+const errorDocument = root + "error.html";
+const port = 3000;
+const previewJsPath =
+  import.meta.url.substring(0, import.meta.url.lastIndexOf("/")) +
+  "/awsw-preview.cjs";
 
 const createResolvablePromise = () => {
   let resolve: (event: "css" | "refresh") => void = () => {};
@@ -13,13 +16,28 @@ const createResolvablePromise = () => {
   return Object.assign(promise, { resolve });
 };
 
+const fileExists = (path: string) =>
+  fs.promises.access(path, fs.constants.F_OK).then(
+    () => true,
+    () => false
+  );
+
 /** A promise that gets resolved once there were changes in the filesystem */
 let fsChangePromise = createResolvablePromise();
+
+let errorDocumentExists = false;
+fileExists(root + errorDocument).then(
+  (exists) => (errorDocumentExists = exists)
+);
 
 // Listen to changes in the filesystem to resolve pending promises
 fs.watch(root, { recursive: true }).addListener("change", (_, filename) => {
   const file = typeof filename === "string" ? filename : filename.toString();
   const event = file.endsWith("css") ? "css" : "refresh";
+  // Re-check if error doc exists now
+  fileExists(root + errorDocument).then(
+    (exists) => (errorDocumentExists = exists)
+  );
   fsChangePromise.resolve(event);
   fsChangePromise = createResolvablePromise();
 });
@@ -33,24 +51,26 @@ const processRequest = async (
     return fsChangePromise.then((event) => ({ status: 200, body: { event } }));
 
   // Open index.html as root file
-  if (path === "") path = "index.html";
+  if (path === "/") path = "index.html";
+  const hasFileExtension = path.includes(".");
   // If there is no file extension assume html
-  if (!path.includes(".")) path += ".html";
+  if (!hasFileExtension) path += ".html";
 
-  try {
-    // TODO append JS logic to refresh page on changes
+  // TODO append JS logic to refresh page on changes
+  if (await fileExists(root + path)) {
     const stream = fs.createReadStream(root + path);
-    stream.push('<script src="//awsw-preview.js" type="module"></script>');
+    if (path.includes(".html") || !hasFileExtension)
+      stream.push('<script src="//awsw-preview.js" type="module"></script>');
     return { status: 200, body: stream };
-  } catch (error) {}
-
-  // Nothing returned yet so it's a 404 error
-  try {
+  }
+  // Resource not in filesystem so it's a 404 error
+  else if (errorDocumentExists) {
     // Try reading the error document as error response
     const stream = fs.createReadStream(root + errorDocument);
     return { status: 404, body: stream };
-  } catch {
-    // No error document so show default message
+  }
+  // Error document doesn't exist so show default message
+  else {
     return {
       status: 404,
       body: {
@@ -69,11 +89,14 @@ const requestListener = async (req: IncomingMessage, res: ServerResponse) => {
     response = await processRequest(path);
   } else response = { status: 400, body: { message: "Missing request URL" } };
 
-  /** Stringified version of `response.body` if it's not a `string` or `ReadStream` */
-  const body =
-    typeof response.body === "object" && !(response.body instanceof ReadStream)
-      ? JSON.stringify(response.body)
-      : response.body;
-  res.writeHead(response.status).end(body);
+  const { body, status } = response;
+  res.writeHead(status);
+
+  if (body instanceof ReadStream) body.pipe(res);
+  else res.end(typeof body === "object" ? JSON.stringify(body) : body);
 };
-export const server = http.createServer(requestListener);
+const server = http.createServer(requestListener);
+const listener = server.listen(port);
+listener.on("listening", () =>
+  console.log(`Preview running on http://localhost:${port}`)
+);
