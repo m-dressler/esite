0ad083721fe5e52a28091c350944e60b3a494044
Mauritz Dressler <mauritz@prodport.com>
2024-02-04 16:16:26 +0100

    📈 build.ts | allow building/publishing all in parallel

--- build.ts
+++ build.ts
@@ -63,13 +63,10 @@ if (!project) {
     type: "list",
     name: "project",
     message: "Select a project to build:",
-    choices: modules,
+    choices: ["all", ...modules],
   })) as { project: (typeof modules)[number] };
   project = result.project;
 }
-
-const projectPath = `./${project}/`;
-const buildFolderName = projectPath + "lib/";
 
 if (!(args.includes("--build") || args.includes("--publish"))) {
   console.error(
@@ -78,112 +75,119 @@ if (!(args.includes("--build") || args.includes("--publish"))) {
   process.exit(1);
 }
 
-const listFiles = (path: string, type?: string | string[]) => {
-  let files = readdirSync(path, {
-    recursive: true,
-    encoding: "utf-8",
-  });
-  if (type) {
-    const options =
-      typeof type === "string" ? ["." + type] : type.map((t) => "." + t);
-    files = files.filter((f) => options.some((o) => f.endsWith(o)));
-  }
-  return files.map((f) => path + f);
-};
+const buildProject = async (projectName: string) => {
+  const projectPath = `./${projectName}/`;
+  const buildFolderName = projectPath + "lib/";
 
-/**
- * @param {string} path
- */
-const clearDirectory = async (path: string) => {
-  await fs.promises
-    .access(path, fs.constants.F_OK)
-    .catch(() => fs.mkdirSync(path));
-  const files = fs.readdirSync(path);
+  const listFiles = (path: string, type?: string | string[]) => {
+    let files = readdirSync(path, {
+      recursive: true,
+      encoding: "utf-8",
+    });
+    if (type) {
+      const options =
+        typeof type === "string" ? ["." + type] : type.map((t) => "." + t);
+      files = files.filter((f) => options.some((o) => f.endsWith(o)));
+    }
+    return files.map((f) => path + f);
+  };
 
-  const removals: Promise<void>[] = [];
-  for (const file of files)
-    removals.push(
-      fs.promises.rm(`${path}/${file}`, { recursive: true, force: true })
-    );
-  await Promise.all(removals);
-};
-
-const tsCompile = async () => {
-  fs.copyFileSync("./tsconfig.json", projectPath + "tsconfig.json");
-  try {
-    await exec("tsc", { cwd: projectPath });
-
-    // Check index.js exists
+  /**
+   * @param {string} path
+   */
+  const clearDirectory = async (path: string) => {
     await fs.promises
-      .access(buildFolderName + "/index.js", fs.constants.F_OK)
-      .catch(async () => {
-        // If it doesn't exist, try to extract folder
-        const moduleFolder = `${buildFolderName}${project}/src/`;
-        const tempFolder = `${projectPath}temp/`;
-        const folderExists = await fs.promises
-          .access(moduleFolder, fs.constants.F_OK)
-          .then(
-            () => true,
-            () => false
-          );
-        fs.rmSync(tempFolder, { recursive: true, force: true });
-        fs.renameSync(moduleFolder, tempFolder);
-        fs.rmSync(buildFolderName, { recursive: true });
-        fs.renameSync(tempFolder, buildFolderName);
-        if (!folderExists) throw { stdout: "Unexpected build error (TS1)" };
-      });
-    fs.rmSync(projectPath + "tsconfig.json");
-  } catch (error) {
-    fs.rmSync(projectPath + "tsconfig.json");
-    if (error && typeof error === "object" && "stdout" in error) {
-      console.error(error.stdout);
-      process.exit(1);
-    } else throw error;
-  }
+      .access(path, fs.constants.F_OK)
+      .catch(() => fs.mkdirSync(path));
+    const files = fs.readdirSync(path);
+
+    const removals: Promise<void>[] = [];
+    for (const file of files)
+      removals.push(
+        fs.promises.rm(`${path}/${file}`, { recursive: true, force: true })
+      );
+    await Promise.all(removals);
+  };
+
+  const tsCompile = async () => {
+    fs.copyFileSync("./tsconfig.json", projectPath + "tsconfig.json");
+    try {
+      await exec("tsc", { cwd: projectPath });
+
+      // Check index.js exists
+      await fs.promises
+        .access(buildFolderName + "/index.js", fs.constants.F_OK)
+        .catch(async () => {
+          // If it doesn't exist, try to extract folder
+          const moduleFolder = `${buildFolderName}${projectName}/src/`;
+          const tempFolder = `${projectPath}temp/`;
+          const folderExists = await fs.promises
+            .access(moduleFolder, fs.constants.F_OK)
+            .then(
+              () => true,
+              () => false
+            );
+          fs.rmSync(tempFolder, { recursive: true, force: true });
+          fs.renameSync(moduleFolder, tempFolder);
+          fs.rmSync(buildFolderName, { recursive: true });
+          fs.renameSync(tempFolder, buildFolderName);
+          if (!folderExists) throw { stdout: "Unexpected build error (TS1)" };
+        });
+      fs.rmSync(projectPath + "tsconfig.json");
+    } catch (error) {
+      fs.rmSync(projectPath + "tsconfig.json");
+      if (error && typeof error === "object" && "stdout" in error) {
+        console.error(error.stdout);
+        process.exit(1);
+      } else throw error;
+    }
+  };
+
+  const minifyScript = async (file: string) => {
+    const content = await fs.promises.readFile(file, { encoding: "utf-8" });
+
+    let { code } = await minify(content, {
+      compress: true,
+      output: { comments: false },
+      mangle: {
+        toplevel: true,
+      },
+      module: true,
+    });
+    if (!code) throw new Error("Couldn't minify " + file);
+    await fs.promises.writeFile(file, code);
+  };
+
+  const minifyJs = async () => {
+    const files = listFiles(buildFolderName, ["js", "cjs"]);
+    await Promise.all(files.map(minifyScript));
+  };
+
+  await clearDirectory(buildFolderName);
+  console.log(projectName + " | Compiling Typescript");
+  await tsCompile();
+  console.log(projectName + " | Minifying scripts");
+  await minifyJs();
 };
 
-const minifyScript = async (file: string) => {
-  const content = await fs.promises.readFile(file, { encoding: "utf-8" });
+const publishProject = async (projectName: string) => {
+  const projectPath = `./${projectName}/`;
+  const getTotp = () => {
+    const secret = process.env.OTP_KEY_HEX;
+    if (!secret) throw new Error("Missing env-var 'OTP_KEY_HEX'");
+    const counterBuffer = Buffer.alloc(8);
+    counterBuffer.writeBigInt64BE(BigInt(Math.floor(Date.now() / 30_000)), 0);
+    const hmac = crypto
+      .createHmac("sha1", Buffer.from(secret, "hex"))
+      .update(counterBuffer)
+      .digest();
+    const offset = hmac[hmac.length - 1] & 0x0f;
+    const otpPart = hmac.readUInt32BE(offset) & 0x7fffffff;
+    const otp = otpPart % Math.pow(10, 6);
+    return otp.toString().padStart(6, "0");
+  };
 
-  let { code } = await minify(content, {
-    compress: true,
-    output: { comments: false },
-    mangle: {
-      toplevel: true,
-    },
-    module: true,
-  });
-  if (!code) throw new Error("Couldn't minify " + file);
-  await fs.promises.writeFile(file, code);
-};
-
-const minifyJs = async () => {
-  const files = listFiles(buildFolderName, ["js", "cjs"]);
-  await Promise.all(files.map(minifyScript));
-};
-
-const getTotp = () => {
-  const secret = process.env.OTP_KEY_HEX;
-  if (!secret) throw new Error("Missing env-var 'OTP_KEY_HEX'");
-  const counterBuffer = Buffer.alloc(8);
-  counterBuffer.writeBigInt64BE(BigInt(Math.floor(Date.now() / 30_000)), 0);
-  const hmac = crypto
-    .createHmac("sha1", Buffer.from(secret, "hex"))
-    .update(counterBuffer)
-    .digest();
-  const offset = hmac[hmac.length - 1] & 0x0f;
-  const otpPart = hmac.readUInt32BE(offset) & 0x7fffffff;
-  const otp = otpPart % Math.pow(10, 6);
-  return otp.toString().padStart(6, "0");
-};
-
-await clearDirectory(buildFolderName);
-console.log("Compiling Typescript");
-await tsCompile();
-console.log("Minifying scripts");
-await minifyJs();
-if (args.includes("--publish")) {
-  console.log("Publishing to npmjs.org");
+  console.log(projectName + " | Publishing to npmjs.org");
   const otp = getTotp();
 
   let command = "pnpm publish --access public --otp " + otp;
@@ -191,4 +195,13 @@ if (args.includes("--publish")) {
 
   const { stdout, stderr } = await exec(command, { cwd: projectPath });
   console.log(stdout, stderr);
+};
+
+if (project === "all") await Promise.all(getModuleNames().map(buildProject));
+else await buildProject(project);
+
+if (args.includes("--publish")) {
+  if (project === "all")
+    await Promise.all(getModuleNames().map(publishProject));
+  else await publishProject(project);
 }
