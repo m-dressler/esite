7a2c8087e084bb043ef1f39edc70ee715adb6c89
Mauritz Dressler <mauritz@prodport.com>
2024-02-04 16:08:55 +0100

    📈 cache-bust | use hash of target file, not targeting file

--- cache-bust/README.md
+++ cache-bust/README.md
@@ -6,7 +6,9 @@ Use [cache-busting](https://www.keycdn.com/support/what-is-cache-busting) to ens
 
 Ensure you have `@awsw/core` installed and add `"cache-bust"` to the `Modules` to your `aws-website-config.yaml` .
 
-Wherever you would like to use cache-busting, simply add `[AWSW_CACHE_BUST]` which @awsw/cache-bust will automatically replace with the file.
+Wherever you would like to use cache-busting, simply add `[AWSW_CACHE_BUST]` or `[AWSW_CACHE_BUST=<PATH_TO_TARGET_FILE>]` which @awsw/cache-bust will automatically replace with the target's hash value.
+
+Important! Omitting the `=<PATH_TO_TARGET_FILE>` part matches the section between the first (`"`, `'`, \`, or `=`) and a `?`. This simplifies working with HTML attributes but means the path must be preceded by any of the four opening tokens, NOT include any of the tokens in the path, AND the `[AWSW_CACHE_BUST]` token must be preceded by a `?` query parameter character.
 
 Example:
 
@@ -14,6 +16,7 @@ Example:
 <html>
   <head>
     <link rel="stylesheet" href="/style.css?v=[AWSW_CACHE_BUST]" />
+    <link rel="stylesheet" href="/script.js?v=[AWSW_CACHE_BUST=/script.js]" />
   </head>
 </html>
 ```
@@ -24,4 +27,4 @@ Example:
   - Description: Specify the token that should be replaced with the file hash to cache-bust
   - Type: string
   - Optional: true
-  - Default: `"[AWSW_CACHE_BUST]"`
+  - Default: `"AWSW_CACHE_BUST"`
--- cache-bust/src/index.ts
+++ cache-bust/src/index.ts
@@ -1,19 +1,25 @@
 import { BuildConfig, Configuration } from "../../core/src";
 import fsSync, { promises as fs } from "fs";
 import crypto from "crypto";
+import path from "path";
 
 export const CustomConfig = {
   CacheBustToken: {
     type: "string",
     optional: true,
-    default: "[AWSW_CACHE_BUST]",
+    default: "AWSW_CACHE_BUST",
   },
 } as const satisfies Configuration;
+
+/** A cache for hashes to not compute them twice */
+const hashes: { [fileName: string]: string } = {};
 
 export const buildConfig: BuildConfig<typeof CustomConfig> = {
   step: 5_000,
   devRequired: false,
   build: async (Config) => {
+    const cacheBustPrefix = "[" + Config.CacheBustToken;
+
     const generateHash = async (filePath: string): Promise<Buffer> => {
       const hash = crypto.createHash("sha1");
       const stream = fsSync.createReadStream(filePath);
@@ -23,13 +29,75 @@ export const buildConfig: BuildConfig<typeof CustomConfig> = {
       );
     };
 
+    const getHash = async (filePath: string) => {
+      if (hashes[filePath]) return hashes[filePath];
+      const hashBuffer = await generateHash(filePath);
+      const hash = hashBuffer.toString("base64url");
+      hashes[filePath] = hash;
+      return hash;
+    };
+
+    const getTargetPath = (
+      filePath: string,
+      fileTarget: string,
+      content: string,
+      index: number
+    ) => {
+      if (fileTarget === "") {
+        // Get file path from attribute
+        const queryEnd = content.lastIndexOf("?", index);
+        const pathStart = Math.max(
+          content.lastIndexOf('"', queryEnd),
+          content.lastIndexOf("'", queryEnd),
+          content.lastIndexOf("`", queryEnd),
+          content.lastIndexOf("=", queryEnd)
+        );
+        fileTarget = "=" + content.substring(pathStart + 1, queryEnd);
+      }
+
+      // Means it targets a specific file
+      if (fileTarget[0] === "=") {
+        // Is absolute path in build folder
+        if (fileTarget[1] === "/")
+          return Config.BuildPath + fileTarget.substring(2);
+        // Is relative path to file
+        else return path.resolve(filePath, fileTarget);
+      }
+
+      const lineNumber =
+        Array.from(content.substring(0, index).matchAll(/\n/g)).length + 1;
+      throw new Error(
+        `Cache-bust | File "${filePath}:${lineNumber}" | Target "${fileTarget}" is invalid`
+      );
+    };
+
     const cacheBust = async (filePath: string) => {
       const content = await fs.readFile(filePath, { encoding: "utf-8" });
-      if (!content.includes(Config.CacheBustToken)) return;
-      const hash = await generateHash(filePath).then((buf) =>
-        buf.toString("base64url")
-      );
-      const updated = content.replaceAll(Config.CacheBustToken, hash);
+      let index = content.indexOf(cacheBustPrefix);
+      if (index === -1) return;
+      let updated = content.substring(0, index);
+      do {
+        const targetStart = index + cacheBustPrefix.length;
+        const endIndex = content.indexOf("]", targetStart);
+        const fileTarget = content.substring(targetStart, endIndex);
+        const targetPath = getTargetPath(filePath, fileTarget, content, index);
+        const targetExists = await fs.stat(targetPath).then(
+          () => true,
+          () => false
+        );
+        if (!targetExists) {
+          const lineNumber = Array.from(updated.matchAll(/\n/g)).length + 1;
+          throw new Error(
+            `Cache-bust | File "${filePath}:${lineNumber}" | Target "${fileTarget}" (resolved "${targetPath}") doesn't exist`
+          );
+        }
+        updated += await getHash(targetPath);
+        index = content.indexOf(cacheBustPrefix, endIndex + 1);
+        updated += content.substring(
+          endIndex + 1,
+          index === -1 ? undefined : index
+        );
+      } while (index !== -1);
       await fs.writeFile(filePath, updated, { encoding: "utf-8" });
     };
 
