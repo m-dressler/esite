ab25ede3bf00b1acb05c70166066adee1ab762d1
Mauritz Dressler <mauritz@prodport.com>
2024-04-04 11:29:58 +0200

    📈 sass | debug logs

--- src/sass/src/index.ts
+++ src/sass/src/index.ts
@@ -6,7 +6,7 @@ export const CustomConfig = {} as const satisfies Configuration;
 export const buildConfig: BuildConfig<typeof CustomConfig> = {
   step: 50_000,
   devRequired: true,
-  build: async (Config) => {
+  build: async (Config, { log }) => {
     const sassCompile = async (fileName: string) => {
       const filePath = Config.BuildPath + fileName;
       try {
@@ -23,12 +23,14 @@ export const buildConfig: BuildConfig<typeof CustomConfig> = {
       }
     };
 
+    log.debug("SASS | Collecting files");
     const files = await fs.readdir(Config.BuildPath, { recursive: true });
 
-    await Promise.all(
-      files
-        .filter((f) => f.endsWith(".scss") || f.endsWith(".sass"))
-        .map((f) => sassCompile(f))
+    const sassFiles = files.filter(
+      (f) => f.endsWith(".scss") || f.endsWith(".sass")
     );
+    log.debug("SASS | Compiling", sassFiles.length, "files");
+    await Promise.all(sassFiles.map((f) => sassCompile(f)));
+    log.debug("SASS | Successfully compiled all SASS files");
   },
 };
