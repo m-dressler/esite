2d0af512ac93278748f5c4a58c35c006b74e6e41
Mauritz Dressler <mauritz@prodport.com>
2024-04-04 11:31:29 +0200

    📈 typescript | debug logs

--- src/typescript/src/index.ts
+++ src/typescript/src/index.ts
@@ -14,19 +14,24 @@ const listTsFiles = async (path: string) => {
 export const buildConfig: BuildConfig<typeof CustomConfig> = {
   step: 50_000,
   devRequired: true,
-  build: async (Config) => {
+  build: async (Config, { log }) => {
+    log.debug("TYPESCRIPT | COPYING tsconfig.json");
     await fs
       .copyFile("./tsconfig.json", Config.BuildPath + "tsconfig.json")
-      // If file doesn't exist ignore and compile with default options 
+      // If file doesn't exist ignore and compile with default options
       .catch(() => {});
 
+    log.debug("TYPESCRIPT | Gathering ts files");
     const tsFilesPromise = listTsFiles(Config.BuildPath);
+    log.debug("TYPESCRIPT | Compiling ts files");
     await exec("tsc", { cwd: Config.BuildPath });
-    
+
+    log.debug("TYPESCRIPT | Removing ts files");
     // Delete all the ts files from build
     const tsFiles = await tsFilesPromise;
     await Promise.all(tsFiles.map((f) => fs.rm(f)));
 
+    log.debug("TYPESCRIPT | Removing tsconfig.json");
     // Delete tsconfig.json if present
     await fs.rm(Config.BuildPath + "tsconfig.json").catch(() => {});
   },
